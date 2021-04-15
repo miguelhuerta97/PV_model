d49e4fc655a2c1fa41f8f2c81e4c5f5a04243314
miguelhuerta97 <66267439+miguelhuerta97@users.noreply.github.com>
2021-04-15 12:28:15 -0400

    Update latex.py

--- latex.py
+++ latex.py
@@ -1,4 +1,5 @@
 def latex(PVModule,metrics,errorPV):
+  import itertools
   f = open(PVModule+".tex", "w")
   f.write(r"""\documentclass[conference]{IEEEtran}
 \usepackage[utf8]{inputenc}
@@ -16,49 +17,36 @@ def latex(PVModule,metrics,errorPV):
 \usepackage{textcomp}
 \usepackage{dsfont}
 \usepackage{empheq}
-
 \usepackage[TABBOTCAP, tight]{subfigure}
 \usepackage{multirow}
-
 \setlength{\marginparwidth}{2cm}
 \usepackage[textsize=scriptsize,shadow]{todonotes}
 \newcommand{\tde}[1]{\todo[inline,color=green!40]{#1}}
 \newcommand{\txcomment}[1]{{\color{red!80}{[#1]}}}
-
 \def\BibTeX{{\rm B\kern-.05em{\sc i\kern-.025em b}\kern-.08em
     T\kern-.1667em\lower.7ex\hbox{E}\kern-.125emX}}
-
 \graphicspath{{./}}
 \newcommand{\unit}[1]{\,\text{\texttt{#1}}}
 \newcommand{\unita}[1]{[\texttt{#1}]}
 \newcommand{\units}[1]{\,#1}
 \newcommand{\uno}{\mathds{1}}
-
 \newcommand{\addfig}[1]{Fig.~\ref{#1}}
 \newcommand{\addtab}[1]{Tab.~\ref{#1}}
 \newcommand{\addeq}[1]{eq.~\ref{#1}}
-
 \DeclareMathOperator*{\argmin}{arg\,min}
 \DeclareMathOperator*{\midoper}{mid}
-
 \graphicspath{{FIgs/Results/%s/}}
-
 \begin{document}
-
 \begin{figure}[!ht]
     \centering
     \includegraphics[height=.82\paperheight]{pv.png}
 \end{figure}
-
 \newpage
-
 \begin{figure}[!ht]
     \centering
     \includegraphics[height=.82\paperheight]{iv.png}
 \end{figure}
-
 \newpage
-
 \begin{figure*}[!ht]
     \centering
     \subfigure[Error curvas PV]{
@@ -66,17 +54,13 @@ def latex(PVModule,metrics,errorPV):
     \subfigure[Error curvas IV]{
     \includegraphics[width=0.8\linewidth]{ivError.png}}
 \end{figure*}
-
 \newpage
-
 \begin{figure*}[!ht]
     \centering
     \subfigure[Traking]{
     \includegraphics[width=0.95\linewidth]{tracking.png}}
 \end{figure*}
-
 \newpage
-
 \begin{figure*}[!ht]
     \centering
     \begin{tabular}{|c|p{0.11\linewidth}|p{0.11\linewidth}|}
@@ -97,18 +81,13 @@ def latex(PVModule,metrics,errorPV):
     \vspace{1em}
     \subfigure[Params]{
     \includegraphics[width=0.9\linewidth]{params.png}}
-
 \end{figure*}
-
 \newpage
-
 \begin{figure*}[!ht]
     \centering
     \subfigure[Error en el conjunto de testeo]{
     \includegraphics[width=0.9\linewidth]{TestError1.png}}
-
   \vspace{1em}
-
   \begin{tabular}{|c|p{0.07\linewidth}|p{0.07\linewidth}|p{0.07\linewidth}|p{0.07\linewidth}|p{0.07\linewidth}|p{0.07\linewidth}|p{0.07\linewidth}|p{0.07\linewidth}|}
     \hline\hline
     \multirow{2}{*}{Variable} 
@@ -132,17 +111,12 @@ def latex(PVModule,metrics,errorPV):
     \hline\hline
   \end{tabular}
 \end{figure*}
-
 \newpage
-
 \begin{figure*}[!ht]
   \centering
-
   \subfigure[Error en el conjunto de testeo]{
     \includegraphics[width=0.9\linewidth]{TestError2.png}}
-
   \vspace{1em}
-
   \begin{tabular}{|c|p{0.06\linewidth}|p{0.06\linewidth}|p{0.06\linewidth}|p{0.06\linewidth}|}
   \hline\hline
   \multirow{2}{*}{Variable} &\multicolumn{4}{c|}{MAE}\\\cline{2-5}
@@ -200,10 +174,9 @@ def latex(PVModule,metrics,errorPV):
       V$_\text{oc}$ & %6.3f & %6.3f & %6.3f& %6.3f\\
   \hline\end{tabular}
 \end{figure*}
-
 \end{document}
   """%(PVModule,
-      *[k1 for k in ['S','T','b','IL','I0','Rs','Gp'] for k1 in ParamsMeanStd[k]],
+      *list(itertools.chain(*[[modelPV.MeanStd['mean'][k], modelPV.MeanStd['std'][k]] for k in ['S','T','b','IL','I0','Rs','Gp']])),
       *[k2 for k1 in ['Isc', 'Pmp', 'Imp', 'Vmp', 'Voc'] for k in ['De Soto', 'Dobos', 'Boyd','Neural Network'] for k2 in metrics[k][k1]],
       *[errorPV[k][k1][k2] for k2 in ['MAE', 'MSE', 'MSLE', 'MAPE'] for k1 in ['Total', 'Isc', 'Pmp', 'Imp', 'Vmp', 'Voc']for k in ['De Soto', 'Dobos', 'Boyd','Neural Network']],
        ))
